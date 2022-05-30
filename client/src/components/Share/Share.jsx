3a03fe0b4a626f8b53dac67fad1e43683549e5e1
Bullet <gerardmartinsegismundo@gmail.com>
2022-05-30 12:20:31 -0700

    modify client, share done.

---
+++ client/src/components/Share/Share.jsx
@@ -0,0 +1,35 @@
+import './share.css'
+import { PermMedia, Label, Room, EmojiEmotions } from '@mui/icons-material'
+
+export default function Share() {
+  return (
+    <div className='share'>
+      <div className='share-wrapper'>
+        <div className='share-top'>
+          <img className='share-profile-img' src='/assets/person/1.jpeg' alt='profile' />
+          <input type='text' placeholder="What's on your mind, Gerard?" className='share-input' />
+        </div>
+        <hr className='share-hr' />
+        <div className='share-bottom'>
+          <div className='share-option'>
+            <PermMedia htmlColor='tomato' className='share-icon' />
+            <span className='share-option-text'>Photo or Video</span>
+          </div>
+          <div className='share-option'>
+            <Label htmlColor='blue' className='share-icon' />
+            <span className='share-option-text'>Tag</span>
+          </div>
+          <div className='share-option'>
+            <Room htmlColor='green' className='share-icon' />
+            <span className='share-option-text'>Location</span>
+          </div>
+          <div className='share-option'>
+            <EmojiEmotions htmlColor='goldenrod' className='share-icon' />
+            <span className='share-option-text'>Feelings</span>
+          </div>
+          <button className='share-button'>Share</button>
+        </div>
+      </div>
+    </div>
+  )
+}
