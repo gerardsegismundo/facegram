9381335352bb7bc3572f7ea970998eb82c3fe8b8
Bullet <gerardmartinsegismundo@gmail.com>
2022-06-26 23:12:26 -0700

    Adding redirect.

--- client/src/components/Rightbar/Rightbar.jsx
+++ client/src/components/Rightbar/Rightbar.jsx
@@ -1,5 +1,23 @@
 import './rightbar.css'
 
 export default function Rightbar() {
-  return <div className='rightbar'>Rightbar</div>
+  return (
+    <div className='rightbar'>
+      <h3>Birthdays</h3>
+      <div className='birthday-container'>
+        <img className='birthday-icon' src='assets/icons/birthday.png' alt='birthday' />
+        <div className='birthday-text'>
+          &nbsp;<b>Gerard Segismundo</b> and <b>7 others friends</b> have birthdays today.
+        </div>
+      </div>
+      <hr />
+      <h3>Contacts</h3>
+      <ul className='rightbar-friendlist'>
+        <li className='rightbar-friend'>
+          <img src='/assets/person/3.jpeg' alt='friend' className='rightbar-profile-img' />
+          <span className='rightbar-online'></span>
+        </li>
+      </ul>
+    </div>
+  )
 }
--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -14,55 +14,53 @@ import {
 export default function Sidebar() {
   return (
     <div className='sidebar'>
-      <div className='sidebar-wrapper'>
-        <ul className='sidebar-list'>
-          <li className='sidebar-list-item'>
-            <RssFeed className='sidebar-icon' />
-            <span className='sidebar-list-item-text'>Feed</span>
-          </li>
+      <ul className='sidebar-list'>
+        <li className='sidebar-list-item'>
+          <RssFeed className='sidebar-icon' />
+          <span className='sidebar-list-item-text'>Feed</span>
+        </li>
 
-          <li className='sidebar-list-item'>
-            <Chat className='sidebar-icon' />
-            <span className='sidebar-list-item-text'>Chats</span>
-          </li>
-          <li className='sidebar-list-item'>
-            <PlayCircleFilledOutlined className='sidebar-icon' />
-            <span className='sidebar-list-item-text'>Videos</span>
-          </li>
-          <li className='sidebar-list-item'>
-            <Group className='sidebar-icon' />
-            <span className='sidebar-list-item-text'>Groups</span>
-          </li>
-          <li className='sidebar-list-item'>
-            <Bookmark className='sidebar-icon' />
-            <span className='sidebar-list-item-text'>Saved</span>
-          </li>
-          <li className='sidebar-list-item'>
-            <HelpOutline className='sidebar-icon' />
-            <span className='sidebar-list-item-text'>Questions</span>
-          </li>
-          <li className='sidebar-list-item'>
-            <WorkOutline className='sidebar-icon' />
-            <span className='sidebar-list-item-text'>Jobs</span>
-          </li>
-          <li className='sidebar-list-item'>
-            <Event className='sidebar-icon' />
-            <span className='sidebar-list-item-text'>Events</span>
-          </li>
-          <li className='sidebar-list-item'>
-            <School className='sidebar-icon' />
-            <span className='sidebar-list-item-text'>Courses</span>
-          </li>
-        </ul>
-        <button className='sidebar-button'>See more</button>
-        <hr className='sidebar-hr' />
-        <ul className='sidebar-friendlist'>
-          <li className='sidebar-friend'>
-            <img src='/assets/person/2.jpeg' alt='friend' className='sidebar-friend-img' />
-            <span className='sidebar-friend-name'>Gerard Pogi</span>
-          </li>
-        </ul>
-      </div>
+        <li className='sidebar-list-item'>
+          <Chat className='sidebar-icon' />
+          <span className='sidebar-list-item-text'>Chats</span>
+        </li>
+        <li className='sidebar-list-item'>
+          <PlayCircleFilledOutlined className='sidebar-icon' />
+          <span className='sidebar-list-item-text'>Videos</span>
+        </li>
+        <li className='sidebar-list-item'>
+          <Group className='sidebar-icon' />
+          <span className='sidebar-list-item-text'>Groups</span>
+        </li>
+        <li className='sidebar-list-item'>
+          <Bookmark className='sidebar-icon' />
+          <span className='sidebar-list-item-text'>Saved</span>
+        </li>
+        <li className='sidebar-list-item'>
+          <HelpOutline className='sidebar-icon' />
+          <span className='sidebar-list-item-text'>Questions</span>
+        </li>
+        <li className='sidebar-list-item'>
+          <WorkOutline className='sidebar-icon' />
+          <span className='sidebar-list-item-text'>Jobs</span>
+        </li>
+        <li className='sidebar-list-item'>
+          <Event className='sidebar-icon' />
+          <span className='sidebar-list-item-text'>Events</span>
+        </li>
+        <li className='sidebar-list-item'>
+          <School className='sidebar-icon' />
+          <span className='sidebar-list-item-text'>Courses</span>
+        </li>
+      </ul>
+      <button className='sidebar-button'>See more</button>
+      <hr className='sidebar-hr' />
+      <ul className='sidebar-friendlist'>
+        <li className='sidebar-friend'>
+          <img src='/assets/person/2.jpeg' alt='friend' className='sidebar-friend-img' />
+          <span className='sidebar-friend-name'>Gerard Pogi</span>
+        </li>
+      </ul>
     </div>
   )
 }
